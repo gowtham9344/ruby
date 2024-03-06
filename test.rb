c7b19f3a0be2820085308f24c11e98b844ede647
gowtham9344 <gowtham@mobicip.com>
2024-03-06 17:15:03 +0530

    modified url regex

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 # Function to find and print URLs in a file
 def find_and_print_urls(file_path)
   
-  pattern = %r{(https://www\.|http://www\.|https://|http://)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?/[a-zA-Z0-9]{2,}|((https://www\.|http://www\.|https://|http://)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https://www\.|http://www\.|https://|http://)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?}i
+  pattern = %r{(https://www\.|http://www\.|https://|http://)?[a-zA-Z\d-]{2,}(\.[a-zA-Z\d-]{2,})(\.[a-zA-Z\d-]{2,})?/[a-zA-Z0-9\d-]{2,}|((https://www\.|http://www\.|https://|http://)?[a-zA-Z\d-]{2,}(\.[a-zA-Z\d-]{2,})(\.[a-zA-Z\d-]{2,})?)|(https://www\.|http://www\.|https://|http://)?[a-zA-Z0-9\d-]{2,}\.[a-zA-Z0-9\d-]{2,}\.[a-zA-Z0-9\d-]{2,}(\.[a-zA-Z0-9\d-]{2,})?}i
 
   File.open(file_path, "r") do |file|
       matches = []
