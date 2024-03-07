ed388ab05843626a8df16a8a4ca0910abbc8de9b
gowtham9344 <153889284+gowtham9344@users.noreply.github.com>
2024-03-07 15:17:48 +0530

    Delete unneeded

--- new1.rb
+++
@@ -1,5 +0,0 @@
-require 'active_record'
-
-# Load the database configuration from database.yml
-ActiveRecord::Base.establish_connection(YAML.load_file('database.yml')[ENV['RACK_ENV'] || 'development'])
-
